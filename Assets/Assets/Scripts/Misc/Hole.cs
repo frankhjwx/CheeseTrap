ca17886a938f610c41ff0a32d3c39fb68864f689
unknown <837989833@qq.com>
2019-01-22 17:54:20 +0800

    17：54

---
+++ Assets/Assets/Scripts/Misc/Hole.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole
+{
+    public float radius {get; set;}
+    public Vector2 position {get; set;}
+    public bool XHole { get; set; }
+    public GameObject gameObject { get; set; }
+}
