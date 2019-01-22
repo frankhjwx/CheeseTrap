b19f70c363232677be3bc5b2fa8cbdea722efff4
frankhjwx <frankhjwx@gmail.com>
2019-01-22 18:35:30 +0800

    fix the branch

--- Assets/Scripts/Managers/HoleManager.cs
+++ Assets/Scripts/Managers/HoleManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,7 +15,7 @@ public class HoleManager : MonoBehaviour
     private int playerNum = 2;
     // holeTexture has the size 960x540
     // holeTexture is used to calculate holes and masks
-    private Texture2D holeTexture;  
+    private Texture2D holeTexture;
 
     // only for debug
     // private GameObject testTextureDisplay;
@@ -29,10 +30,9 @@ public class HoleManager : MonoBehaviour
         HoleTextureInitialize();
 
 
-        CreateHole(new Vector2(9f, 10.4f), 0.5f, 1);
-        CreateHole(new Vector2(10f, 5.4f), 0.6f, 2);
-        CreateHole(new Vector2(9.6f, 4.6f), 0.6f, 2);
-        CreateHole(new Vector2(9f, 4f), 0.8f, 1);
+        //CreateHole(new Vector2(9f, 10.8f), 0.5f, 1);
+        //CreateHole(new Vector2(10f, 5.4f), 0.6f, 2);
+        //CreateHole(new Vector2(9f, 4f), 0.8f, 1);
         Debug.Log(areas[1]);
         Debug.Log(areas[2]);
         // UpdateHoleTexture(new Vector2(960, 480), 50, 255);
@@ -44,13 +44,15 @@ public class HoleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DisplayHoleTexture();
     }
 
-    private void HoleTextureInitialize(){
+    private void HoleTextureInitialize()
+    {
         holeTexture = new Texture2D(texWidth, texHeight);
-        Color[] colors = new Color[texWidth*texHeight];
-        for (int i=0; i<texWidth*texHeight; i++){
+        Color[] colors = new Color[texWidth * texHeight];
+        for (int i = 0; i < texWidth * texHeight; i++)
+        {
             colors[i] = Color.black;
         }
         holeTexture.SetPixels(0, 0, texWidth, texHeight, colors);
@@ -62,10 +64,11 @@ public class HoleManager : MonoBehaviour
         // sr = testTextureDisplay.AddComponent<SpriteRenderer>();
         // Sprite pic = Sprite.Create(holeTexture, new Rect(0, 0, texWidth, texHeight), new Vector2(0.5f,0.5f));
         // sr.sprite = pic;
-        
+
     }
 
-    public int CreateHole(Vector2 position, float radius, int playerID){
+    public int CreateHole(Vector2 position, float radius, int playerID)
+    {
         Hole hole = new Hole();
         hole.position = position;
         hole.radius = radius;
@@ -80,16 +83,19 @@ public class HoleManager : MonoBehaviour
         return holes.Count;
     }
 
-    public void UpdateHole(int holeID, Vector2 position, float radius, int playerID){
-        holes[holeID].position = position;
-        holes[holeID].radius = radius;
-        holes[holeID].gameObject.GetComponent<CircleCollider2D>().radius = radius;
-        holes[holeID].gameObject.GetComponent<CircleCollider2D>().transform.position = position;
+    public void UpdateHole(int id, Vector2 position, float radius, int playerID)
+    {
+        //Debug.Log(id);
+        holes[id-1].position = position;
+        holes[id-1].radius = radius;
+        holes[id-1].gameObject.GetComponent<CircleCollider2D>().radius = radius;
+        holes[id - 1].gameObject.GetComponent<CircleCollider2D>().transform.position = position;
         UpdateHoleTexture(position, radius, playerID);
         return;
     }
 
-    private void UpdateHoleTexture(Vector2 position, float radius, int playerID){
+    private void UpdateHoleTexture(Vector2 position, float radius, int playerID)
+    {
         position.x *= 50;
         position.y *= 50;
         radius *= 50;
@@ -101,32 +107,32 @@ public class HoleManager : MonoBehaviour
 
         if (Left < 0)
             Left = 0;
-        if (Right >= texWidth)
-            Right = texWidth - 2;
+        if (Right > texWidth)
+            Right = texWidth;
         if (Bottom < 0)
             Bottom = 0;
-        if (Top >= texHeight)
-            Top = texHeight - 2;
-        Debug.Log(Top);
-        Debug.Log(Bottom);
+        if (Top > texHeight)
+            Top = texHeight;
 
         int colorIndex = 0;
-        for (int x = Left; x <= Right; x++){
-            for (int y = Bottom; y <= Top; y++){
-                if ((x - position.x)*(x - position.x) + (y - position.y)*(y - position.y) <= radius*radius && holeTexture.GetPixel(x, y).r == 0){
+        for (int x = Left; x <= Right; x++)
+        {
+            for (int y = Bottom; y <= Top; y++)
+            {
+                if ((x - position.x) * (x - position.x) + (y - position.y) * (y - position.y) <= radius * radius && holeTexture.GetPixel(x, y).r == 0)
+                {
                     holeTexture.SetPixel(x, y, new Color32((byte)playerID, 0, 0, 255));
                     areas[playerID]++;
-                    
                 }
 
                 colorIndex++;
             }
         }
         holeTexture.Apply();
-        DisplayHoleTexture();
     }
 
-    public void DisplayHoleTexture(){
+    public void DisplayHoleTexture()
+    {
         GameObject map = GameObject.Find("Map");
         map.GetComponent<SpriteRenderer>().material.SetTexture("_Mask", holeTexture);
         GameObject shadow = GameObject.Find("HoleShadow");
