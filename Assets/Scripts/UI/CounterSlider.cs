a13d5ba8a285413e9abd327d640e31d0600bdefb
ReaNAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2019-01-23 15:59:02 +0800

    做UI中，存档

---
+++ Assets/Scripts/UI/CounterSlider.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterSlider : MonoBehaviour
+{
+    public Slider leftSlider;
+    public Slider rightSlider;
+    public float leftValue = 0;
+    public float rightValue = 0;
+
+    private float currentPosition;
+    private float truePosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftSlider.minValue = 0;
+        leftSlider.maxValue = 1;
+        rightSlider.minValue = 0;
+        rightSlider.maxValue = 1;
+        leftSlider.value = 0.5f;
+        rightSlider.value = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/InGameCountUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameCountUI : MonoBehaviour
+{
+
+    public Slider eatAmountComparison;
+    public Slider weightA, weightB;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        eatAmountComparison.value = 0.5f;
+        weightA.value = 0;
+        weightB.value = 0;
+    }
+
+    public void AddEatAmount(int playerID, float amountDelta)
+    {
+        
+    }
+}
