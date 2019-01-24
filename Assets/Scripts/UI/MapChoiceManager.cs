d9a00e26172cb3dedd301d993f1efd6d1999584e
ReaNAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2019-01-24 15:26:15 +0800

    暂存

--- Assets/Scripts/UI/MapChoiceManager.cs
+++ Assets/Scripts/UI/MapChoiceManager.cs
@@ -11,11 +11,14 @@ public class MapChoiceManager : MonoBehaviour
     public MiceChoiceUI mapChoice;
 
     private MiceBasicInfo[] basicInfo = new MiceBasicInfo[2];
+    private JSONNode root;
     
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this);
+        var miceInfo = Resources.Load<TextAsset>("MiceInfo").text;
+        root = JSON.Parse(miceInfo);
         
     }
 
@@ -23,5 +26,22 @@ public class MapChoiceManager : MonoBehaviour
     {
         if (playerID == 1 || playerID == 2) return basicInfo[playerID - 1];
         else return basicInfo[0];
+    }
+
+    public int GetMapChosen()
+    {
+        return mapChoice.CurrentChoice;
+    }
+
+    private void GetInfo()
+    {
+        var choice1Node = root[p1Choice.CurrentChoice];
+        basicInfo[0] = new MiceBasicInfo(choice1Node["choiceID"].AsInt, choice1Node["speedState1"].AsFloat, 
+            choice1Node["speedState2"].AsFloat, choice1Node["speedState3"].AsFloat, choice1Node["speedState4"].AsFloat,
+            choice1Node["eatThresholdMin"].AsFloat, choice1Node["eatThresholdMid"].AsFloat, choice1Node["eatThresholdMax"].AsFloat);
+        var choice2Node = root[p2Choice.CurrentChoice];
+        basicInfo[1] = new MiceBasicInfo(choice2Node["choiceID"].AsInt, choice2Node["speedState1"].AsFloat, 
+            choice2Node["speedState2"].AsFloat, choice2Node["speedState3"].AsFloat, choice2Node["speedState4"].AsFloat,
+            choice2Node["eatThresholdMin"].AsFloat, choice2Node["eatThresholdMid"].AsFloat, choice2Node["eatThresholdMax"].AsFloat);
     }
 }
--- Assets/Scripts/player/MiceBasicInfo.cs
+++ Assets/Scripts/player/MiceBasicInfo.cs
@@ -12,4 +12,17 @@ public class MiceBasicInfo
     public float eatThresholdMin;
     public float eatThresholdMid;
     public float eatThresholdMax;
+
+    public MiceBasicInfo(int choiceID, float speedState1, float speedState2, float speedState3, float speedState4,
+        float eatThresholdMin, float eatThresholdMid, float eatThresholdMax)
+    {
+        this.choiceID = choiceID;
+        this.speedState1 = speedState1;
+        this.speedState2 = speedState2;
+        this.speedState3 = speedState3;
+        this.speedState4 = speedState4;
+        this.eatThresholdMin = eatThresholdMin;
+        this.eatThresholdMid = eatThresholdMid;
+        this.eatThresholdMax = eatThresholdMax;
+    }
 }
