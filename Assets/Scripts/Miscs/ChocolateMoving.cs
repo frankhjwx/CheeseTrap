664d47b52c62e4ba515b2ffead4b4e5eb0797141
ReaNAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2019-01-25 11:56:38 +0800

    少量修改

---
+++ Assets/Scripts/Miscs/ChocolateMoving.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChocolateMoving : MonoBehaviour
+{
+    public Vector2 StartPoint;
+    public Vector2 EndPoint;
+    public float circleTime = 2.0f;
+
+    private float timer = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localPosition = new Vector3(StartPoint.x, StartPoint.y, StartPoint.y - 0.7f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+}
