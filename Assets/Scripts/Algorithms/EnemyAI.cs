d8c8dcfefa7434efd5479f8fdb9853c0ae47448a
unknown <837989833@qq.com>
2019-04-16 16:15:35 +0800

    add debug of 4 directions

--- Assets/Scripts/Algorithms/EnemyAI.cs
+++ Assets/Scripts/Algorithms/EnemyAI.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 public class EnemyAI : MonoBehaviour
 {
     private Collider2D playerCollider;
-    Dictionary<Vector2, int> scaninfo = new Dictionary<Vector2, int>();//vector2为本地坐标，玩家位置为原点
-    public int w=48,h=27;
+
+    public int w=20,h=20;
     public float density = 2;
     private float die = 0;
     private float normal = 0;
     private float ice = 0;
     public HoleManager holeManager;
-    enum terrainchoose
-    {
-        die,normal,ice
-    }
-    private terrainchoose tc;
 
     public void Awake()
     {
@@ -25,82 +20,51 @@ public class EnemyAI : MonoBehaviour
     public void Update()
     {
         ScanAround();
-        scaninfo.Clear();
     }
 
     public void ScanAround()
     {
-        die = 0;normal = 0;ice = 0;
+        //Dictionary<Vector2, int> scaninfo = new Dictionary<Vector2, int>();//vector2为本地坐标，玩家位置为原点
+        float up = 0; float down = 0; float right = 0; float left = 0;
         Vector2 playerposition = playerCollider.transform.position;
-        for(int i=-w; i<w; i++)
+        for(int i=-w; i<-3; i++)
         {
-                int terrainfo = holeManager.getTerrainStatus(new Vector2(playerposition.x + i * density, playerposition.y + j * density));
-                Debug.Log(terrainfo);
-                switch (terrainfo)
-                {
-                    case -1:
-                        die++;
-                        break;
-                    case 0:
-                        normal++;
-                        break;
-                    case 1:
-                        ice++;
-                        break;
-                }
-                scaninfo.Add(new Vector2(i, 0), terrainfo);
-        }
-        for (int i = -w; i < w; i++)
-        {
-            int terrainfo = holeManager.getTerrainStatus(new Vector2(playerposition.x + i * density, playerposition.y + j * density));
-            Debug.Log(terrainfo);
-            switch (terrainfo)
+                int terrainfo = holeManager.getTerrainStatus(new Vector2(playerposition.x + i * density, playerposition.y));
+            if (terrainfo ==-1)
             {
-                case -1:
-                    die++;
-                    break;
-                case 0:
-                    normal++;
-                    break;
-                case 1:
-                    ice++;
-                    break;
+                float add= Mathf.Abs(i);
+                left += Mathf.Sqrt(add);
             }
-            scaninfo.Add(new Vector2(i, 0), terrainfo);
-
+                //scaninfo.Add(new Vector2(i, 0), terrainfo);
         }
+        for (int i =w; i >3; i--)
+        {
+            int terrainfo = holeManager.getTerrainStatus(new Vector2(playerposition.x + i * density, playerposition.y));
+            if (terrainfo == -1)
+            {
+                float add = Mathf.Abs(i);
+                right += Mathf.Sqrt(add);
+            }
+        }
+        for (int i = -h; i < -3; i++)
+        {
+            int terrainfo = holeManager.getTerrainStatus(new Vector2(playerposition.x , playerposition.y + i * density));
+            if (terrainfo == -1)
+            {
+                float add = Mathf.Abs(i);
+                down += Mathf.Sqrt(add);
+            }
+        }
+        for (int i = h; i >3; i--)
+        {
+            int terrainfo = holeManager.getTerrainStatus(new Vector2(playerposition.x, playerposition.y + i * density));
+            if (terrainfo == -1)
+            {
+                float add =Mathf.Abs(i);
+                up += Mathf.Sqrt(add);
+            }
+        }
+        Debug.Log("up"+up+"    down"+down+"     left"+left+"    right"+right);
     }
 
-    public Vector2 Turning()
-    {
-        die =die* 0.1f;
-        ice =ice* 0.2f;
-        float rd= Random.Range(0,die+normal+ice);
-        if(rd<=normal)
-        {
-            tc = terrainchoose.normal;
-        }
-        if(rd<ice+normal)
-        {
-            tc = terrainchoose.ice;
-        }
-        if (rd < die + normal + ice)
-        {
-            tc = terrainchoose.die;
-        }
-        switch(tc)
-        {
-            case terrainchoose.normal:
-
-                break;
-            case terrainchoose.ice:
-
-                break;
-            case terrainchoose.die:
-
-                break;
-        }
-        Vector2 turnDirection= new Vector2();
-        return turnDirection;
-    }
 }
