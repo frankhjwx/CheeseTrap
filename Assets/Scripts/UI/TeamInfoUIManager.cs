888d250733eac4f4d196b76adcb622734f7fce52
ReaNAiveD <32201005+ReaNAiveD@users.noreply.github.com>
2019-05-10 03:51:54 +0800

    修改了teaminfo的鬼东西

--- Assets/Scripts/UI/TeamInfoUIManager.cs
+++ Assets/Scripts/UI/TeamInfoUIManager.cs
@@ -5,10 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class TeamInfoUIManager : MonoBehaviour
 {
+    private AudioManager audioManager;
+
+    private void Awake(){
+        audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+    }
     private void Update()
     {
         if (Input.GetButtonDown("P1 Cancel") || Input.GetButtonDown("P2 Cancel"))
         {
+            audioManager.PlayOnceAudioByPath("audio/buttonOnClick");
             Back();
         }
     }
